4560dcd4129884fbd2a9df2c492301a2830b6a8e
Pratyusha Routh <pratyusharouth2001@gmail.com>
2022-01-18 00:03:21 +0530

    ✨ Navbar

---
+++ src/components/TopNav.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Container, Navbar, Nav } from "react-bootstrap";
+import "./comp.css"
+
+const TopNav = () => {
+
+    return (
+            <Navbar collapseOnSelect expand="lg" sticky="top" className="topnav">
+                <Container>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" style={{color:"transparent"}}/>
+                    <Navbar.Collapse id="responsive-navbar-nav"  style={{color:"red"}}>
+                        <Nav className="me-auto" style={{color:"red"}}>
+                            <a href="#about">About Me</a>
+                            <a href="#skills">Skills</a>
+                            <a href="#accomp">Accomplishments</a>
+                            <a href="#contact">Contact Me</a>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+    );
+}
+
+export default TopNav;
